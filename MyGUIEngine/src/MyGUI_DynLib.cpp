fecf54ad22dd22d94331883ad86ff4185f1c3635
Vasily Pankratov <foverzar@gmail.com>
2024-11-07 19:25:03 +0300

    Apply dlfcn-fix-mygui.patch from GBATemp

--- MyGUIEngine/src/MyGUI_DynLib.cpp
+++ MyGUIEngine/src/MyGUI_DynLib.cpp
@@ -7,12 +7,6 @@
 #include "MyGUI_Precompiled.h"
 #include "MyGUI_DynLib.h"
 
-#if MYGUI_PLATFORM == MYGUI_PLATFORM_WIN32
-#	include <windows.h>
-#elif MYGUI_PLATFORM == MYGUI_PLATFORM_LINUX
-#	include <dlfcn.h>
-#endif
-
 namespace MyGUI
 {
 	DynLib::DynLib(const std::string& name) :
@@ -31,22 +25,10 @@ namespace MyGUI
 		MYGUI_LOG(Info, "Loading library " << mName);
 
 		std::string name = mName;
-#if MYGUI_PLATFORM == MYGUI_PLATFORM_WIN32
-		const std::string extension = ".dll";
-#elif MYGUI_PLATFORM == MYGUI_PLATFORM_LINUX
-		const std::string extension = ".so";
-#else
 		const std::string extension = "";
-#endif
 
 		if (!extension.empty() && name.find(extension) == std::string::npos)
 			name += extension;
-
-#if MYGUI_PLATFORM == MYGUI_PLATFORM_APPLE
-		//APPLE SPECIFIC CODE HERE
-#else
-		mInstance = (MYGUI_DYNLIB_HANDLE)MYGUI_DYNLIB_LOAD( name.c_str() );
-#endif
 
 		return mInstance != 0;
 	}
@@ -56,47 +38,16 @@ namespace MyGUI
 	{
 		// Log library unload
 		MYGUI_LOG(Info, "Unloading library " << mName);
-#if MYGUI_PLATFORM == MYGUI_PLATFORM_APPLE
-		//APPLE SPECIFIC CODE HERE
-#else
-		if (MYGUI_DYNLIB_UNLOAD(mInstance))
-		{
-			MYGUI_EXCEPT("Could not unload dynamic library '" << mName << "'. System Error: " << dynlibError());
-		}
-#endif
 	}
 
 	void* DynLib::getSymbol( const std::string& strName ) const throw()
 	{
-#if MYGUI_PLATFORM == MYGUI_PLATFORM_APPLE
-		//APPLE SPECIFIC CODE HERE
 		return nullptr;
-#else
-		return (void*)MYGUI_DYNLIB_GETSYM(mInstance, strName.c_str());
-#endif
 	}
 
 	std::string DynLib::dynlibError() const
 	{
-#if MYGUI_PLATFORM == MYGUI_PLATFORM_WIN32
-		LPVOID lpMsgBuf;
-		FormatMessage(
-			FORMAT_MESSAGE_ALLOCATE_BUFFER |
-			FORMAT_MESSAGE_FROM_SYSTEM |
-			FORMAT_MESSAGE_IGNORE_INSERTS,
-			NULL,
-			GetLastError(),
-			MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
-			(LPTSTR) &lpMsgBuf,
-			0,
-			NULL);
-		std::string ret = (char*)lpMsgBuf;
-		// Free the buffer.
-		LocalFree( lpMsgBuf );
-		return ret;
-#else
 		return "no unix error function defined yet";
-#endif
 	}
 
 	std::string DynLib::getName(void) const
